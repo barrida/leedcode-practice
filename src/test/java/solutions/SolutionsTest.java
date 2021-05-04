90ffc63f609843cc5dc2c19e6cb4d010a3340067
barrida <suleymanube@gmail.com>
2021-05-04 18:02:49 +0100

    maxSubArray with Kadane's Algorithm

--- src/test/java/solutions/SolutionsTest.java
+++ src/test/java/solutions/SolutionsTest.java
@@ -135,8 +135,12 @@ public class SolutionsTest {
 
     @Test
     public void maxSubArray() {
+        assertEquals(11, Solutions.maxSubArray(new int[]{6, 3, 1, -7, 8}));
+        assertEquals(9, Solutions.maxSubArray(new int[]{6, 3, -10, -7, 8}));
+        assertEquals(25, Solutions.maxSubArray(new int[]{6, 3, 1, 7, 8}));
         assertEquals(6, Solutions.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
         assertEquals(23, Solutions.maxSubArray(new int[]{5, 4, -1, 7, 8}));
         assertEquals(1, Solutions.maxSubArray(new int[]{1}));
+        assertEquals(-1, Solutions.maxSubArray(new int[]{-5, -4, -1, -7, -8}));
     }
 }
